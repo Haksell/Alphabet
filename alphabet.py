3724686e4fd5bd3bf92c6f70c44848ec8e45b70a
Haksell <axel.brisse@gmail.com>
2020-05-12 02:58:34 +0200

    Clearer function name get_avg -> truncated_mean

--- alphabet.py
+++ alphabet.py
@@ -5,7 +5,7 @@ import sys
 from time import time
 
 
-def get_avg(results, *, size, skip):
+def truncated_mean(results, *, size, skip):
     if size > len(results):
         return math.inf
     counting = sorted(results[-size:])[skip:-skip]
@@ -29,8 +29,8 @@ while True:
         single = math.inf
     results.append(single)
     # Stats
-    avg5 = get_avg(results, size=5, skip=1)
-    avg25 = get_avg(results, size=25, skip=2)
+    avg5 = truncated_mean(results, size=5, skip=1)
+    avg25 = truncated_mean(results, size=25, skip=2)
     best_single = min(single, best_single)
     best_avg5 = min(avg5, best_avg5)
     best_avg25 = min(avg25, best_avg25)
